ff96056077f345f50174069b8f6407a98ce7e5e6
Kiran Crawford <kirancrawf@gmail.com>
2018-06-26 18:49:29 +0100

    Add background picture, tidy code

---
+++ head.js
@@ -0,0 +1,16 @@
+document.write('\
+<!--Import Google Icon Font-->\
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">\
+    <!--Import materialize.css 1.0.0.alpha-->\
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-alpha.3/css/materialize.min.css">\
+\
+\
+    <!--Let browser know website is optimized for mobile-->\
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>\
+\
+    <link rel="icon" href="images/favicon.ico">\
+    <title>Durham University Mountaineering Club Home</title>\
+    <meta name="description" content="Durham University Mountaineering Club">\
+    <meta name="keywords" content="durham, university, mountaineering, club, dumc, climbing, bouldering">\
+    <meta name="author" content="admin">\
+')
