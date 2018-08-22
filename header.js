2f48f4b065b49dbdfce101c71364713c14e0792b
Kiran-Crawford <kirancrawf@gmail.com>
2018-08-22 18:16:18 +0100

    padding changes

--- header.js
+++ header.js
@@ -17,7 +17,7 @@ document.write('\
     <div class="navbar-fixed">\
         <nav>\
         <div class="nav-wrapper purple darken-2">\
-            <a href="index.html" class="brand-logo"><img src="images/logos/DUMC/bwtight.png" height="55" ></a>\
+            <a href="index.html" class="brand-logo"><img src="images/Logos/DUMC/bwtight.png" height="55" ></a>\
             <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>\
             <ul class="right hide-on-med-and-down">\
                 <li><a href="join.html">Join</a></li>\
