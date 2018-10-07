6c11c204c2a8dd4352ee67ff89705799edab0834
Kiran-Crawford <kirancrawf@gmail.com>
2018-10-07 21:08:41 +0100

    add facebook icon

--- header.js
+++ header.js
@@ -23,7 +23,7 @@ document.write('\
                 <li><a href="join.html">Join</a></li>\
                 <!-- Dropdown Trigger -->\
                 <li><a id=dropdown-trigger1 class="dropdown-trigger" href="#!" data-target="dropdown1">Club Info<i class="material-icons right">arrow_drop_down</i></a></li>\
-                <li><a href="https://www.facebook.com/groups/DUMC.Forum/">Facebook</a></li>\
+                <li><a href="https://www.facebook.com/groups/DUMC.Forum/"><i class="fab fa-facebook"></i> Facebook</a></li>\
             </ul>\
             <!-- mobile navigation side bar -->\
             <ul class="sidenav" id="mobile-demo">\
