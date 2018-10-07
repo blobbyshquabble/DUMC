ad8b03e425dbc2abbb5d03d3a9ea8eebdefe7b0f
Kiran-Crawford <kirancrawf@gmail.com>
2018-10-07 21:05:35 +0100

    update footer

--- footer.js
+++ footer.js
@@ -2,19 +2,22 @@ document.write('\
 <div class="container">\
 <div class="row">\
   <div class="col l4 s12 center-align">\
-    <img class="responsive-img center" src="images/Logos/DU/TeamDurham.gif" alt="Team Durham Logo">\
+  <a href="https://www.teamdurham.com/universitysport/university_clubs/mountaineering/" target="_blank">\
+    <img class="responsive-img center" src="images/Logos/DU/TeamDurham.gif" alt="Team Durham Logo" style="max-height: 100px;"></a>\
   </div>\
   <div class="col l4 offset-4 s12 center-align">\
-  <img class="responsive-img center" src="images/Logos/BMC.gif" alt="BMC Logo">\
+  <a href="https://www.thebmc.co.uk/" target="_blank">\
+  <img class="responsive-img center" src="images/Logos/BMC.gif" alt="BMC Logo" style="max-height: 100px;"></a>\
   </div>\
   <div class="col l4 offset-8 s12 center-align">\
-  <img class="responsive-img" src="images/Logos/dcclogo.BMP" alt="Durham Climbing Centre Logo">\
+  <a href="http://www.durhamclimbingcentre.co.uk/" target="_blank">\
+  <img class="responsive-img" src="images/Logos/dcclogo.BMP" alt="Durham Climbing Centre Logo" style="max-height: 100px;"></a>\
   </div>\
 </div>\
 </div>\
 <div class="footer-copyright">\
 <div class="container">\
-© 2018 Durham University Mountaineering Club\
+&copy; 2018 Durham University Mountaineering Club\
 <a class="grey-text text-lighten-4 right" href="#!">More Links</a>\
 </div>\
 </div>\
